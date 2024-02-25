3933a1b81f50542d359f82607d0ed387d9151860
Apple <apple@Apples-MacBook-Pro-2.local>
2024-02-25 18:09:58 +0530

    1st Day of Learning

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Chai from './chai'
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Bngyi to Pile na bhadwe</h1>
+    </>
+  )
+}
+
+export default App
